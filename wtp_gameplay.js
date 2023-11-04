a5029164889b5e48d303225bb1701c629a49300e
Citralyn <130531955+Citralyn@users.noreply.github.com>
2023-11-04 13:46:06 -0700

    added images

--- wtp_gameplay.js
+++ wtp_gameplay.js
@@ -4,10 +4,10 @@ let playerScore = 0;
 let timeLeft = 60;
 const quiz_bank = [
     {
-        question: "8-bit Petr?",
+        question: "Shrek Petr?",
         answers: [
             {
-                text: "8-bit Petr",
+                text: "Shrek Petr",
                 correct: true,
             },
             {
@@ -22,10 +22,11 @@ const quiz_bank = [
                 text: "Zelda Petr",
                 correct: false,
             },
-        ]
+        ],
+        image: "https://ih1.redbubble.net/image.3546877029.0615/st,small,507x507-pad,600x600,f8f8f8.jpg"
     }, 
     {
-        question: "Lucky Petr?",
+        question: "Bob Ross Petr?",
         answers: [
             {
                 text: "Green Petr",
@@ -40,20 +41,21 @@ const quiz_bank = [
                 correct: false,
             },
             {
-                text: "Lucky Petr",
+                text: "Bob Ross Petr",
                 correct: true,
             },
-        ]
+        ],
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRQUmMj8WqDBrqra-IovGTsDES6mOf8ehhuQ&usqp=CAU"
     },
     {
-        question: "Thanos Petr?",
+        question: "Ursula Petr?",
         answers: [
             {
                 text: "8-bit Petr",
                 correct: false,
             },
             {
-                text: "Thanos Petr",
+                text: "Ursula Petr",
                 correct: true,
             },
             {
@@ -64,25 +66,40 @@ const quiz_bank = [
                 text: "Zelda Petr",
                 correct: false,
             },
-        ]
+        ],
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc-_iTDXCF9kQtL4PTabmtp_M8-QDom0x_rw&usqp=CAU"
     }
 ]
 
-function loaded(){
+function newQuestion() {
+    document.getElementById('quiz').innerHTML = ""; 
     const current = quiz_bank[Math.floor(Math.random() * 3)];
 
-    document.getElementById('quiz').innerHTML += `
-    <div>
-        ${current.question}
-        <div>
-         <button onclick="selected(${current.answers[0].correct})">${current.answers[0].text}</button>
-         <button onclick="selected(${current.answers[1].correct})">${current.answers[1].text}</button>
-         <button onclick="selected(${current.answers[2].correct})">${current.answers[2].text}</button>
-         <button onclick="selected(${current.answers[3].correct})">${current.answers[3].text}</button>
+document.getElementById("image").innerHTML = `
+<image src="${current.image}"></image>
+`; 
 
-        </div>
+
+document.getElementById('quiz').innerHTML += `
+
+<div>
+    ${current.question}
+    <div>
+     <button onclick="selected(${current.answers[0].correct})">${current.answers[0].text}</button>
+     <button onclick="selected(${current.answers[1].correct})">${current.answers[1].text}</button>
+     <button onclick="selected(${current.answers[2].correct})">${current.answers[2].text}</button>
+     <button onclick="selected(${current.answers[3].correct})">${current.answers[3].text}</button>
+
     </div>
-    `;
+</div>
+`;
+
+}
+
+
+function loaded(){
+
+    newQuestion(); 
 
     let timer = setInterval(function() {
         timeLeft -= 1;
@@ -94,10 +111,18 @@ function loaded(){
     }, 1000);
 }
 
+function clearQuestion() {
+    document.getElementById("${questionNumber}").innerHTML += ""; 
+}
+
 function selected(correct){
     console.log("user seleced correct value",correct)
-    if(correct) playerScore+=1;
+    if(correct) {
+        playerScore+=1;
+    } 
+
     updateScore();
+    newQuestion(); 
 }
 
 // buttons.forEach(button => button.addEventListener("click", value)) {
